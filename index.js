813d7913049244f620ce69252bb4b0988762591b
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-07 17:42:35 +0600

    added options in projection to data fetch

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const app =express();
+const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -18,44 +18,56 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    const roomsCollection = client.db('innSight').collection('rooms');
+        const roomsCollection = client.db('innSight').collection('rooms');
 
-    app.get('/rooms',async (req,res)=>{
-        const cursor = roomsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/rooms', async (req, res) => {
+            const cursor = roomsCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/rooms/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options ={
+                projection:{id:1, roomType:1, image:1, price: 1, description: 1, pricePerNight: 1, roomSize: 1, availability: 1, images: 1, specialOffers: 1, reviews: 1 }
+            }
+
+            const result = await roomsCollection.findOne(query);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('innsight server is running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`innsight server is running on port ${port}`);
 })
