d0c1180520c13640079f3eb7230ac03b942d5077
Eanur Lihan <lihan.lihu@gmail.com>
2023-11-07 16:38:02 +0600

    conncted with frontend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS);
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9xsrko.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,12 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const roomsCollection = client.db('innSight').collection('rooms');
+
+    app.get('/rooms',async (req,res)=>{
+        const cursor = roomsCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
